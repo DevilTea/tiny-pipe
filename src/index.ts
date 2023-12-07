b59a8c6b399998598c3eb6f56823837176e16864
DevilTea <ch870814@gmail.com>
2023-12-07 17:14:17 +0800

    refactor: improve readability

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,114 @@
-type IsNever<T> = [T] extends [never] ? true : false
 type AnyFn = (...params: any[]) => any
-type MaybePromise<T = any> = T | Promise<T extends infer TT ? TT : never>
+type MaybePromise<T = any> = T | Promise<T>
 type AwaitedReturnType<Fn extends AnyFn> = Awaited<ReturnType<Fn>>
 type IsAsyncFn<Fn extends AnyFn> = ReturnType<Fn> extends infer T
 	? (T extends Promise<any> ? true : false)
 	: never
+type IfTrue<T extends boolean, Then, Else> = T extends true ? Then : Else
+type IfFalse<T extends boolean, Then, Else> = T extends false ? Then : Else
+type ConditionalType<Condition extends boolean, True, False, _Boolean> = IfTrue<
+	Condition,
+	True,
+	IfFalse<
+		Condition,
+		False,
+		_Boolean
+	>
+>
+type IsNever<T> = [T] extends [never] ? true : false
 
-type ConditionalType<Condition extends boolean, True, False, _Boolean> = Condition extends true ? True : Condition extends false ? False : _Boolean
-type SafePipeData<Result, Reason> = { status: 'resolved', result: Result } | { status: 'rejected', reason: Reason }
+type SafeWrappedData<Result, Reason> = { status: 'resolved', result: Result } | { status: 'rejected', reason: Reason }
 type ToSafePipeHandler<
-    Fn extends AnyFn,
-    Reason = unknown,
-    ReturnData extends SafePipeData<AwaitedReturnType<Fn>, Reason> = SafePipeData<AwaitedReturnType<Fn>, Reason>,
+	Fn extends AnyFn,
+	Reason = unknown,
+	ReturnData extends SafeWrappedData<AwaitedReturnType<Fn>, Reason> = SafeWrappedData<AwaitedReturnType<Fn>, Reason>,
 > = (...params: Parameters<Fn>) => ConditionalType<
-    IsAsyncFn<Fn>,
-    Promise<ReturnData>,
-    ReturnData,
-    MaybePromise<ReturnData>
+	IsAsyncFn<Fn>,
+	Promise<ReturnData>,
+	ReturnData,
+	MaybePromise<ReturnData>
 >
 
-type PipeHandler<Last extends PipeHandler = never> = (...params: IsNever<Last> extends true ? [any?] : (IsNever<AwaitedReturnType<Last>> extends true ? [void] : [AwaitedReturnType<Last>])) => any
+type PipeHandlerParams<Last extends PipeHandler> = IfTrue<
+	IsNever<Last>,
+	[any?],
+	IfTrue<
+		IsNever<AwaitedReturnType<Last>>,
+		[void],
+		[AwaitedReturnType<Last>]
+	>
+>
+type PipeHandler<Last extends PipeHandler = never> = (...params: PipeHandlerParams<Last>) => any
 type ReturnedPipeBuilder<
-    First extends PipeHandler,
-    HasAsync extends boolean,
-    Fn extends AnyFn,
+	First extends PipeHandler,
+	HasAsync extends boolean,
+	Fn extends AnyFn,
 > = PipeBuilder<
-    IsNever<First> extends true ? Fn : First,
-    Fn,
-    HasAsync extends true ? true : IsAsyncFn<Fn>
+	IsNever<First> extends true ? Fn : First,
+	Fn,
+	HasAsync extends true ? true : IsAsyncFn<Fn>
 >
 
 class PipeBuilder<
-    First extends PipeHandler = never,
-    Last extends PipeHandler = never,
-    HasAsync extends boolean = false,
+	First extends PipeHandler = never,
+	Last extends PipeHandler = never,
+	HasAsync extends boolean = false,
 > {
 	private _fnList: AnyFn[] = []
 
-	static _createSafePipeData(status: 'resolved' | 'rejected', data: any) {
+	private static _createSafePipeData(status: 'resolved' | 'rejected', data: any) {
 		return status === 'resolved'
 			? { status, result: data }
 			: { status, reason: data }
+	}
+
+	private static _invokeHandler<Last extends PipeHandler, Handler extends PipeHandler<Last>>(handler: Handler, promiseOrValue: any) {
+		return promiseOrValue instanceof Promise
+			? promiseOrValue.then(value => handler(value))
+			: handler(promiseOrValue)
 	}
 
 	/**
 	 * Add a handler to the pipe
 	 */
 	pipe<Handler extends PipeHandler<Last>>(handler: Handler) {
-		this._fnList.push((inputPromiseOrValue: any) => {
-			return inputPromiseOrValue instanceof Promise
-				? inputPromiseOrValue.then(value => handler(value))
-				: handler(inputPromiseOrValue)
+		this._fnList.push((promiseOrValue: any) => {
+			return PipeBuilder._invokeHandler(handler, promiseOrValue)
 		})
-		return this as ReturnedPipeBuilder<First, HasAsync, Handler>
+
+		type ReturnedBuilder = ReturnedPipeBuilder<First, HasAsync, Handler>
+		return this as ReturnedBuilder
 	}
 
 	/**
 	 * Add a handler to the pipe, and handle the error automatically
 	 */
 	pipeSafe<Reason, Handler extends PipeHandler<Last> = PipeHandler<Last>>(handler: Handler) {
-		this._fnList.push((inputPromiseOrValue: any) => {
+		this._fnList.push((promiseOrValue: any) => {
 			try {
-				const outputPromiseOrValue = inputPromiseOrValue instanceof Promise
-					? inputPromiseOrValue.then(value => handler(value))
-					: handler(inputPromiseOrValue)
-
-				return outputPromiseOrValue instanceof Promise
-					? outputPromiseOrValue
+				const unsafeResult = PipeBuilder._invokeHandler(handler, promiseOrValue)
+				return unsafeResult instanceof Promise
+					? unsafeResult
 						.then(result => PipeBuilder._createSafePipeData('resolved', result))
 						.catch(reason => PipeBuilder._createSafePipeData('rejected', reason))
-					: PipeBuilder._createSafePipeData('resolved', outputPromiseOrValue)
+					: PipeBuilder._createSafePipeData('resolved', unsafeResult)
 			}
 			catch (reason) {
 				return PipeBuilder._createSafePipeData('rejected', reason)
 			}
 		})
-		return this as ReturnedPipeBuilder<First, HasAsync, ToSafePipeHandler<Handler, Reason>>
+
+		type ReturnedBuilder = ReturnedPipeBuilder<First, HasAsync, ToSafePipeHandler<Handler, Reason>>
+		return this as ReturnedBuilder
 	}
 
 	/**
 	 * Get the executable function of the pipe
 	 */
 	getExecutable() {
-		return (...params: Parameters<First>): HasAsync extends true ? Promise<AwaitedReturnType<Last>> : ReturnType<Last> => [...this._fnList].reduce((v, f) => f(v), params[0])
+		type FnReturnedValue = HasAsync extends true ? Promise<AwaitedReturnType<Last>> : ReturnType<Last>
+		return (...params: Parameters<First>): FnReturnedValue => [...this._fnList]
+			.reduce((v, f) => f(v), params[0])
 	}
 
 	/**
